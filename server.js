0c9414aed74449c8a02d647e992064d0ad3a7385
Toheeb <thoraf20@gmail.com>
2022-03-16 18:51:52 +0100

    install dependencies and create server

---
+++ server.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+
+import cors from "cors";
+
+import connectDB from "./src/db/connect.js";
+
+app.use(express.json());
+
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
